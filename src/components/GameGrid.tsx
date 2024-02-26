dbd86813e690abd331561366475a7064af709b58
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-02-26 16:04:28 +0100

    clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ export default function GameGrid({ gameQuery }: Props) {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding={10} spacing={3}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={"10px"} spacing={6}>
         {isLoading && skeletons.map(skeleton =>
           <GameCardContainer key={skeleton} >
             <GameCardSkeleton />
