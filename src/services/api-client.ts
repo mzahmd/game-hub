7add65f094c2e4de3bd5858eeeaf7d28dbd0a76b
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-02-26 17:16:05 +0100

    hide key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "304ddeb652ec47259815f6fe23d9183d"
+    key: import.meta.env.VITE_KEY
   }
 })
