279f240e84a2a8c61df93992f85ffceab1973740
mahme001 <minhaz.ahmed@student.hs-rm.de>
2024-03-07 16:34:47 +0100

    fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -22,7 +22,7 @@ export default function GenreList({ onSelectGenre, selectedGenre }: Props) {
     <>
       <Heading fontSize={"2xl"} marginBottom={3}>Genres</Heading>
       <List>
-        {data.map(genre =>
+        {data?.results.map(genre =>
           <ListItem key={genre.id} paddingY={"5px"}>
             <HStack>
               <Image boxSize={"32px"} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} objectFit={"cover"} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import theme from './theme.ts'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
